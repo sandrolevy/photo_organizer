e880f4468f537a43276fde57052d3ab11d217c43
Sandro Levy <sandrojunior.peng@poli.ufrj.br>
2019-09-21 18:53:53 -0300

    Create the photo_organizer.py and .gitignore file

---
+++ photo_organizer.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+import mimetypes
+from datetime import datetime
+from PIL import Image
+
+class PhotoOrganizer:
+
+    def folder_path_from_photo_date(self, file):
+        date = self.photo_shooting_date(file)
+        return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
+
+    def photo_shooting_date(self, file):
+        photo = Image.open(file)
+        info = photo._getexif()
+        if 36867 in info:
+            date = info[36867]
+            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+        else:
+            date = datetime.fromtimestamp(os.path.getmtime(file))
+        return date
+
+    def move_photo(self, file):
+        new_folder = self.folder_path_from_photo_date(file)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+            shutil.move(file,new_folder + '/' + file)
+
+    def organize_photos(self):
+        all_photos = [
+            filename for filename in os.listdir('.') if any(filename.endswith(ext) for ext in self.get_extensions_for_type())
+        ]
+        for filename in all_photos:
+            self.move_photo(filename)
+
+    def get_extensions_for_type(self):
+        general_type = 'image'
+        image_extensions = []
+        for ext in mimetypes.types_map:
+            if mimetypes.types_map[ext].split('/')[0] == general_type:
+                image_extensions.append(ext)
+        return image_extensions
+
+PO = PhotoOrganizer()
+PO.organize_photos()
+
+
+
+
